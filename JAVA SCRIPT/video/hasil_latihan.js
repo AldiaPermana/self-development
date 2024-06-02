b4db39b9a52808f24e7a331321036503dc1c6e01
unknown <aldiapermana99@gmail.com>
2024-06-02 22:49:40 +0700

    bahan Ajar JS 1

---
+++ JAVA SCRIPT/video/hasil_latihan.js
@@ -0,0 +1,47 @@
+// let nama = 'aldia permana' //string
+// let usia = 30 //integer
+// let tinggiBadan = 173.5 // doble float
+// let beratBadan
+// let pacar = 2345
+
+// beratBadan= 60
+
+// if (pacar==null){
+//     pacar = 'belum punya'
+// }else {
+//     pacar = 'udah ada'
+// }
+// let saldoAwal = 50000
+// let saldoTambahan = 80000
+// const hutang = 30000
+// const nilaiSaldoAkhir = saldoAwal + saldoTambahan - hutang
+
+// alert (`nama saya ${nama} usia saya ${usia} tinggi badan saya ${tinggiBadan} berat badan saya ${beratBadan} dan pacar saya ${pacar}`)
+// alert (`saldo awal saya sebesar Rp.${saldoAwal} dan saldo tambahan yang akan saya miliki sebesar Rp.${saldoTambahan} jadi total saldo yang saya miliki sebanyak ${nilaiSaldoAkhir}`)
+
+// // materi kedua array
+// let namaGuru = ['jodi','akbar','rahman']
+// namaGuru.push('dea','aldi')
+// namaGuru.shift()
+// namaGuru.pop()
+// alert (namaGuru)
+
+// //for lop
+// //tiga statemen
+// const namaGuru = ['dean', 'fikri', 'retno', 'farhand']
+// for(let i = 0; i < namaGuru.length; i++){
+//     console.log(namaGuru[i])
+// }
+
+// let i = 10
+// while (i<10) {
+//     i++
+//     console.log('aldipermana')
+// }
+
+// do {
+//     console.log('aldipermana')
+//     i++
+// } while (i < 10)
+
+
